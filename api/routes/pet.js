86d4e1cc1cc64b717358c9f957babd8409b36fbf
vmc9 <victor_mc@live.ca>
2020-05-09 09:10:15 -0400

    additional database updates on pet

--- api/routes/pet.js
+++ api/routes/pet.js
@@ -6,31 +6,45 @@ const Pet = require('../models/pet');
 //Pet GET methods
 router.get('/', (req, res, next) => {
     Pet.find()
+    .exec()
     .then( result => {
-        res.status(200).json({
-            Message: "Response to GET from /pet",
-            pets: result
-        })
+        if(result.length > 0){
+            res.status(200).json({
+                Message: "All pets found",
+                pets: result
+            })
+        } else {
+            res.status(200).json({
+                Message: "Empty database",
+            })
+        }
     })
     .catch( err => {
         res.status(404).json({
-            Message: "No pets found"
+            Message: "No database found"
         })
     });
 });
 
 router.get('/:petId', (req, res, next) => {
     const id = req.params.petId;
-    Pet.findById(id)
+    Pet.findById({ _id: id })
+    .exec()
     .then( result => {
-        res.status(200).json({
-            Message: "Response to GET from /pet",
-            pet: result
-        })
+        if(result){
+            res.status(200).json({
+                Message: "Pet found",
+                pet: result
+            })
+        } else {
+            res.status(404).json({
+                Message: "Pet not found"
+            })
+        }
     })
     .catch( err => {
-        res.status(404).json({
-            Message: "Pet not found"
+        res.status(500).json({
+            Message: err
         })
     });
 });
@@ -48,23 +62,53 @@ router.post('/', (req, res, next) => {
     })
     .catch( error => console.log(err))
     res.status(201).json({
-        Message: "Response to POST from /pet",
+        Message: "Pet added succesfully",
         createdPet: pet
     })
 });
 
+//Pet PATCH methods
 router.patch('/:petId', (req, res, next) => {
     const id = req.params.petId;
-    res.status(200).json({
-        Message: "Response to PATCH from /pet",
+    Pet.update({ _id: id }, { $set: {
+        //TO-DO
+    }})
+    .exec()
+    .then( result => {
+        res.status(200).json({
+            Message: "Pet update succesfull",
+            pet: result
+        })
     })
+    .catch( err => {
+        res.status(500).json({
+            error: err
+        })
+    })
+
 });
 
+//Pet DELETE methods
 router.delete('/:petId', (req, res, next) => {
     const id = req.params.petId;
-    res.status(200).json({
-        Message: "Response to DELETE from /pet",
+    Pet.deleteOne({ _id: id })
+    .exec()
+    .then( result => {
+        if (result.deletedCount === 1){
+            res.status(200).json({
+                Message: "Pet deleted successfully",
+            })
+        } else {
+            res.status(404).json({
+                Message: "Delete failed",
+            })
+        }
     })
+    .catch(err => {
+        res.status(500).json({
+            error: err,
+        })
+    });
 });
 
 module.exports = router;
